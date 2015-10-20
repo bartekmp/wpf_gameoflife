dc747b1c6d8ad169a306a047a3bdf8829dd1ed17
bartek <bartekmp@outlook.com>
2015-10-21 00:51:22 +0200

    added Cell user control and speed dialog

---
+++ GameOfLife/CellControl.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameOfLife
+{
+    /// <summary>
+    /// Interaction logic for CellControl.xaml
+    /// </summary>
+    public partial class CellControl : UserControl
+    {
+        readonly Cell modelCell = new Cell();
+        public CellControl()
+        {
+            InitializeComponent();
+            DataContext = modelCell;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            modelCell.ChangeState();
+           // Btn.Background = modelCell.FillBrush;
+        }
+
+        public bool IsAlive
+        {
+            get { return modelCell.IsAlive; }
+            set { modelCell.IsAlive = value; }
+        }
+        public bool Used
+        {
+            get { return modelCell.Used; }
+            set { modelCell.Used = value; }
+        }
+
+        public void ChangeState() { modelCell.ChangeState();}
+
+        public void SetState(bool state) { modelCell.SetState(state);}
+
+        public void SetDead() { modelCell.SetDead();}
+    }
+}
